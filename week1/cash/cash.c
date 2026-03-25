9ec91321d847d35df95ae08b410694099d66f2b9
dserafin@EP271 <davi@mmati.com.br>
2026-03-25 11:05:31 -0300

    final commit

--- week1/cash/cash.c
+++ week1/cash/cash.c
@@ -1,15 +1,48 @@
 #include <stdio.h>
 #include <cs50.h>
 
+int dollar = -1;
+
 int main(void)
 {
     // get input from user
-    float received = get_float("Change owed: ");
+    while( dollar < 0)
+    {
+        dollar = get_int("Change owed: ");    
+    }
+    
+    // converting float to int
+    int received = dollar; //* 100; 
+
 
     // how many 0.25 cents coins
-    int c25 = (received / 0.25);
-    printf("%i\n", c25);
-    // overrun
-    received = received - (0.25 * c25);    
-    
+    int c25 = (received / 25);
+    if(c25>=1)
+    {
+        printf("%i $ 0.25 coins\n", c25);
+        received = received - (25 * c25);
+    }
+    // how many 0.10 cents coins
+    int c10 = (received / 10);
+    if(c10>=1)
+    {    
+        printf("%i $ 0.10 coins\n", c10);
+        received = received - (10 * c10);
+    }
+    // how many 0.05 cents coins
+    int c5 = (received / 5);
+    if(c5>=1)
+    {
+        printf("%i $ 0.05 coins\n", c5);
+        received = received - (5 * c5);
+    }
+    // how many 0.01 cents coins
+    int c1 = (received / 1);
+    if(c1>=1)
+    {
+        printf("%i $ 0.01 coins\n", c1);
+        received = received - (1 * c1);
+    }
+    int sum = c25+c10+c5+c1;
+    printf ("total of %i coins\n", sum);
 }
